bf7e7e4d33db9134e2fa09b8209646767fec6646
rks1212 <rks1212@users.noreply.github.com>
2019-08-28 23:46:14 +0530

    Update connections.tf

--- connections.tf
+++ connections.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  credentials = "${file("../account.json")}"
+  credentials = "${file("/Users/ravi/Terraform/account.json")}"
   project = "ansible-gce-244616"
   region = "asia-south1-a"
 }
