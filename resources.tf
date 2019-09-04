600ca98a10404057434a1f50cc323debfc19cb1d
rks1212 <rks1212@users.noreply.github.com>
2019-09-04 22:02:38 +0530

    Update resources.tf

--- resources.tf
+++ resources.tf
@@ -12,3 +12,23 @@ resource "aws_subnet" "subnet1" {
   cidr_block = "${cidrsubnet(aws_vpc.AWS_dev_network.cidr_blcok, 3, 1)}"
   vpc_id = "${aws_vpc.AWS_dev_network.id}"
   availability_zone = "us-east-2"
+  
+resource "aws_subnet" "subnet2" {
+  cidr_block = "${cidrsubnet(aws_vpc.AWS_dev_network.cidr_blcok, 2, 2)}"
+  vpc_id = "${aws_vpc.AWS_dev_network.id}"
+  availability_zone = "us-east-2"
+  
+resource "aws_security_group" "subnet_security_group" {
+  vpc_id = "${aws_vpc.AWS_dev_network.id}"
+  
+  ingress {
+    cidr_blocks = [
+      "${aws_vpc.AWS_dev_network.cidr_block}"
+     ]
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    }
+  }
+  
+  
