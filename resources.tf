02a7207f3b6ca6a24b6a8e83a32ae1b0a6a6d106
rks1212 <rks1212@users.noreply.github.com>
2019-09-04 22:26:34 +0530

    Update resources.tf

--- resources.tf
+++ resources.tf
@@ -9,13 +9,13 @@ resource "aws_vpc" "AWS_dev_network" {
   enable_dns_support = true
 }
 resource "aws_subnet" "subnet1" {
-  cidr_block = "${cidrsubnet(aws_vpc.AWS_dev_network.cidr_blcok, 3, 1)}"
+  cidr_block = "${cidrsubnet(aws_vpc.AWS_dev_network.cidr_block, 3, 1)}"
   vpc_id = "${aws_vpc.AWS_dev_network.id}"
   availability_zone = "us-east-2"
   }
   
 resource "aws_subnet" "subnet2" {
-  cidr_block = "${cidrsubnet(aws_vpc.AWS_dev_network.cidr_blcok, 2, 2)}"
+  cidr_block = "${cidrsubnet(aws_vpc.AWS_dev_network.cidr_block, 2, 2)}"
   vpc_id = "${aws_vpc.AWS_dev_network.id}"
   availability_zone = "us-east-2"
   }
