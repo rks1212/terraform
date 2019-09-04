dc257b3dd387ec8453b15e2109a4546571c30e19
rks1212 <rks1212@users.noreply.github.com>
2019-09-04 21:50:48 +0530

    Update resources.tf

--- resources.tf
+++ resources.tf
@@ -11,4 +11,4 @@ resource "aws_vpc" "AWS_dev_network" {
 resource "aws_subnet" "subnet1" {
   cidr_block = "${cidrsubnet(aws_vpc.AWS_dev_network.cidr_blcok, 3, 1)}"
   vpc_id = "${aws_vpc.AWS_dev_network.id}"
-  availability_zone = "us-east-1"
+  availability_zone = "us-east-2"
